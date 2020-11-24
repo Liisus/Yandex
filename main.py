59bc7259d2def410ad81bdc066d99e965c429503
Liisus <48231326+Liisus@users.noreply.github.com>
2020-11-24 21:43:32 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import sys
 
 from random import random
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(527, 422)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 83, 22))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Button"))
+
+
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.b = False
         self.initUI()
 
@@ -30,8 +48,8 @@ class MyWidget(QMainWindow):
 
     def draw_flag(self, qp):
         if self.b:
-            qp.setBrush(QColor(255, 255, 0))
-            a = 200 * random()
+            qp.setBrush(QColor(int(255 * random()), int(255 * random()), int(255 * random())))
+            a = int(250 * random())
             qp.drawEllipse(int(527 * random()), int(422 * random()), a, a)
         else:
             self.b = True
